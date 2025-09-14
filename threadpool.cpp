076d048c4993ef9681ccb219e48f1e737b8c69c3
qinyanjun <yanjun.qin@sharpa.com>
2025-09-14 22:17:46 +0800

    完成Fixed 模式线程池（简陋版）

--- threadpool.cpp
+++ threadpool.cpp
@@ -27,9 +27,18 @@ void ThreadPool::setTaskQueMaxThreshHold(int threshold){
     taskQueMaxThreshHold_ = threshold; 
 }
 // 提交任务
-void ThreadPool::submitTask(std::shared_ptr<Task> sp){
-
-
+Result ThreadPool::submitTask(std::shared_ptr<Task> sp){
+    std::unique_lock<std::mutex> lock(taskQueMtx_); 
+    if(!notFull_.wait_for(lock,std::chrono::seconds(1),[this]{
+        return taskQue_.size() < taskQueMaxThreshHold_;
+    })){
+        std::cout<<"taskQue is full wait_for timeout "<<std::endl;
+        return Result(sp,false);
+    }
+    taskQue_.emplace(sp);
+    taskSize_.fetch_add(1);
+    notEmpty_.notify_all(); 
+    return Result(sp);
 }
 
 
@@ -50,11 +59,39 @@ void ThreadPool::start(int initThreadSize){
 }
 // 线程函数
 void ThreadPool::threadFunc(){
-    std::cout<<"begin threadFunc tid"<<std::this_thread::get_id()
-        <<std::endl;
+    // std::cout<<"begin threadFunc tid"<<std::this_thread::get_id()
+    //     <<std::endl;
 
-    std::cout<<"end threadFunc tid"<<std::this_thread::get_id()
-        <<std::endl;
+    // std::cout<<"end threadFunc tid"<<std::this_thread::get_id()
+    //     <<std::endl;
+    for(;;){
+        std::unique_lock<std::mutex> lock(taskQueMtx_);
+        std::cout<<"tid:"<<std::this_thread::get_id()<<" wait task"<<std::endl;
+        if(!notEmpty_.wait_for(lock,std::chrono::seconds(100),[this]{
+            return taskQue_.size() > 0;
+        })){
+            std::cout<<"taskQue is empty wait_for timeout "<<std::endl;
+            continue;
+        }
+        std::cout<<"tid:"<<std::this_thread::get_id()<<" got task"<<std::endl;
+        std::shared_ptr<Task> task = taskQue_.front();
+        taskQue_.pop();
+        taskSize_.fetch_sub(1);
+        // 如果依然有任务，则继续通知消费者消费
+        // 感觉没啥必要再通知一下
+        if(taskSize_ > 0){
+            notEmpty_.notify_all(); // 
+        }
+        // 通知生产者可以继续生产
+        notFull_.notify_all();
+
+        // 释放锁
+        lock.unlock();
+        // 执行任务
+        if(task){
+            task->exec();
+        }
+    }
 }
  
 //---------线程方法实现--------------
@@ -71,4 +108,61 @@ void Thread::start(){
 
 }
 
+// ------------Task 实现--------------    
+void Task::exec(){
+    // 执行任务 
+    if(result_ != nullptr){
+        result_->setValue(run()); 
+    }
+}
+Task::Task(){
+    result_ = nullptr;
+}
+void Task::setResult(Result* result){
+    result_ = result;
+} 
 
+// ------------Result 实现--------------    
+Result::Result(std::shared_ptr<Task> task,bool isValid):task_(task),isValid_(isValid){
+    task_->setResult(this); 
+}
+Any Result::get(){
+    if(!isValid_){
+        // 是针对线程池的提交任务的成功与否的判断 
+        return "";
+    }
+    // 等待任务执行完成，返回结果 
+    // 任务如果没有执行完，会阻塞在这里，等待任务执行完成 
+    // 如果任务执行完了，会从任务中获取结果  count_用于条件判断无需唤醒 
+    sem_.wait();
+    return std::move(any_);
+}
+
+void Result::setValue(Any any){
+    // 设置结果值 子线程设置结果值
+    this->any_ = std::move(any);
+    sem_.post();
+}
+
+// Result::Result(Result&& other):any_(std::move(other.any_)),sem_(std::move(other.sem_)),task_(std::move(other.task_)){
+//     other.isValid_ = false;
+// }
+// Result& Result::operator=(Result&& other){
+//     if(this != &other){
+//         any_ = std::move(other.any_);
+//         sem_ = std::move(other.sem_);
+//         task_ = std::move(other.task_);
+//     }
+//     return *this;
+// }
+
+// ------------Semaphore 实现--------------     
+Semaphore::Semaphore(Semaphore&& other):count_(std::move(other.count_)){
+    other.count_ = 0;
+}
+Semaphore& Semaphore::operator=(Semaphore&& other){
+    if(this != &other){
+        count_ = std::move(other.count_);
+    }
+    return *this;
+}
--- threadpool.h
+++ threadpool.h
@@ -16,13 +16,101 @@ enum class PoolMode{
     MODE_CACHE, 
 };
 
+class Any{
+public:
+    Any() = default;
+    ~Any() = default;
+    Any(const Any& other)=delete;
+    Any& operator=(const Any& other)=delete;
+    Any(Any&&) = default;
+    Any& operator=(Any&&) = default;
+
+    template<typename T>
+    Any(T data):base_(std::make_unique<Derived<T>>(data)){}
+
+    template<typename T>
+    T cast_(){
+        Derived<T>* ptr = dynamic_cast<Derived<T>*>(base_.get());
+        if(ptr == nullptr){
+            throw std::bad_cast();
+        }
+        return ptr->data_;
+    }
+
+private:
+    // 基类 多态特性实现接受不同类型的数据 
+    class Base{
+    public:
+        virtual ~Base() = default;
+    };
+    // 派生类 接受数据
+    template<typename T>
+    class Derived : public Base{
+    public:
+        Derived(T data):data_(data){}
+        T data_;
+    };
+
+private:
+    std::unique_ptr<Base> base_;
+};
+
+class Semaphore{
+public:
+    Semaphore(int count = 0):count_(count){}
+    ~Semaphore() = default;
+    Semaphore(const Semaphore& other)=delete;
+    Semaphore& operator=(const Semaphore& other)=delete;
+    Semaphore(Semaphore&& other);
+    Semaphore& operator=(Semaphore&& other);
+    void wait(){
+        std::unique_lock<std::mutex> lock(mtx_);
+        cv_.wait(lock, [this]{ return count_ > 0; });
+        count_--;
+    }
+
+    void post(){
+        std::unique_lock<std::mutex> lock(mtx_);
+        count_++;
+        cv_.notify_all();
+    }
+private:
+    int count_;
+    std::mutex mtx_;
+    std::condition_variable cv_;
+};
+class Task;
+// result 
+class Result{
+public:
+    Result(std::shared_ptr<Task> task,bool isValid = true);
+    ~Result() = default;
+    Result(const Result& other)=delete;
+    Result& operator=(const Result& other)=delete;
+    // Result(Result&& other);
+    // Result& operator=(Result&& other); 
+    Any get();
+    void setValue(Any any);
+private:
+    Any any_;
+    Semaphore sem_; 
+    std::shared_ptr<Task> task_; 
+    std::atomic_bool isValid_; 
+};
+
 //
 class Task{
 public:
-    virtual void run()=0; 
-
-
+    Task();
+    ~Task() = default;
+    virtual Any run()=0; 
+    // 封装执行任务函数，用于返回结果 
+    void exec();
+    void setResult(Result* result);
+private:
+    Result* result_;
 };
+
 class Thread{
 public:
     using ThreadFunc = std::function<void()>;
@@ -49,7 +137,7 @@ public:
     // 设置任务队列大小
     void setTaskQueMaxThreshHold(int threshold);
     // 提交任务
-    void submitTask(std::shared_ptr<Task> sp);
+    Result submitTask(std::shared_ptr<Task> sp);
 
     //禁止拷贝赋值！ 
     ThreadPool(const ThreadPool&) = delete;
